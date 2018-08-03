fa349d9d3ca845041ae7f99d661a4a74426c43ed
iGor Polyakov <igor_polyakov@phystech.edu>
2018-08-03 13:48:13 +0300

    new filtering method added

--- src/app/site/projects/projects.component.ts
+++ src/app/site/projects/projects.component.ts
@@ -9,27 +9,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
   xps = [];
+  xpsFiltered = [];
   selectedXP: any;
   videoUrl: any;
   projectsLimit = 3;
   isButtonShown = true;
+  types = [
+    {
+      ru: "Всё",
+      en: "all"
+    },
+    {
+      ru: "Веб",
+      en: "web"
+    },
+    {
+      ru: "Графика",
+      en: "graphics"
+    },
+    {
+      ru: "Текст",
+      en: "text"
+    },
+    {
+      ru: "Остальное",
+      en: "other"
+    }
+  ];
+  selectedType = 'web';
+  selectedBest = true;
 
   constructor(private http: HttpService, private sanitizer: DomSanitizer) {}
 
   ngOnInit() {
     // Наполнение портфолио работами
     this.http.get("assets/data/projects.json").subscribe(
-      data => {
-        this.xps = data;
+      xps => {
+        this.xps = xps;
+        // console.log(xps);
         this.selectedXP = this.xps[0];
+        this.filterWorks();
       },
       error => console.log(error)
     );
   }
 
-  showAllProjects() {
-    this.projectsLimit = this.xps.length;
-    this.isButtonShown = false;
+  filterWorks() {
+    const xpEditable = this.copyObject(this.xps);
+    this.xpsFiltered = [];
+    for (let xp of xpEditable) {
+      const worksFiltered = [];
+      for (let work of xp.works) {
+        if ((!this.selectedBest || work.best) &&
+            (this.selectedType === 'all' || this.selectedType === work.type)) {
+          worksFiltered.push(work);
+        }
+      }
+      if (worksFiltered.length > 0) {
+        xp.works = worksFiltered;
+        this.xpsFiltered.push(xp);
+      }
+    }
   }
 
   openModal(work) {
@@ -39,5 +79,21 @@ export class ProjectsComponent implements OnInit {
     if (this.selectedXP.video !== undefined) {
       this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.selectedXP.video);
     }
+  }
+
+  selectType(event, type) {
+    event.preventDefault();
+    this.selectedType = type;
+    this.filterWorks();
+  }
+
+  toggleBest() {
+    // event.preventDefault();
+    this.selectedBest = !this.selectedBest;
+    this.filterWorks();
+  }
+
+  copyObject(object) { // https://scotch.io/bar-talk/copying-objects-in-javascript
+    return JSON.parse(JSON.stringify(object));
   }
 }
