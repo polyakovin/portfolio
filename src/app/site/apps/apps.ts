7a1e6e5987317e94a1f8cfbc6337895b4c11071b
Igor Polyakov <web@polyakovin.ru>
2022-09-05 01:06:19 +0300

    apps icons added

--- src/app/site/apps/apps.ts
+++ src/app/site/apps/apps.ts
@@ -3,40 +3,48 @@ export default [
     title: 'Попробуй всё',
     url: 'https://polyakovin.github.io/want-smth-test/',
     description: 'Сервис для подбора идеи провести время',
+    icon: 'try-everything',
   },
   {
     title: 'Мой день',
     url: 'https://polyakovin.github.io/daily-organizer/',
     description: 'Менеджер задач на текущий день',
+    icon: 'my-day',
   },
   {
     title: 'Memit',
     url: 'https://polyakovin.github.io/memit-prod/',
     description: 'Менеджер повторяющихся задач',
+    icon: 'memit',
   },
   {
     title: 'TTS Reader',
     url: 'https://polyakovin.github.io/tts-reader/',
     description: 'Читалка текстовых книг вслух',
+    icon: 'tts-reader',
   },
   {
     title: 'Тренажёр РЖЯ',
     url: 'https://polyakovin.github.io/learn-sign-lang/',
     description: 'Как «Stamina», только учим жестовый язык',
+    icon: 'learn-sign-lang',
   },
   {
     title: 'OnLearn',
     url: 'https://polyakovin-on-learn.herokuapp.com/',
     description: 'Сборник обучающих материалов',
+    icon: 'on-learn',
   },
   {
     title: 'Режим дня',
     url: 'https://polyakovin.github.io/franklinTable/',
     description: 'Менеджер по отработке привычек',
+    icon: 'franklin-table',
   },
   {
     title: 'Генератор заявок',
     url: 'https://polyakovin.github.io/bastionRequest/',
     description: 'Шаблон заявок на установку дверей',
+    icon: 'bastion-application',
   },
 ];
