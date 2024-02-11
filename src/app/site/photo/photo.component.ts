4adb6953826bfd546e582b9715821e8f57f692a8
Igor Polyakov <web@polyakovin.ru>
2024-02-11 17:52:48 +0300

    notion link updated

--- src/app/site/photo/photo.component.ts
+++ src/app/site/photo/photo.component.ts
@@ -27,26 +27,26 @@ export class PhotoComponent implements OnInit {
       <p>
         Это небольшой портал, на котором можно найти
         <ul>
-          <li>мои <a href="https://humorous-myrtle-078.notion.site/b4761a0713ff40cc98f842fe502c8145?v=fff551ba13a64bd8803bca5a376eb090" target="_blank">публикации</a>,</li>
+          <li>мои <a href="https://polyakovin.notion.site/b4761a0713ff40cc98f842fe502c8145?v=fff551ba13a64bd8803bca5a376eb090" target="_blank">публикации</a>,</li>
           <li>проекты, над которыми я трудился,</li>
           <li>и <a href="#contacts">контакты</a> для связи со мной.</li>
         </ul>
       </p>
       <p>
-        А вот ещё и <a href="https://humorous-myrtle-078.notion.site/e62ff7b80e3d4fec85cde132852386ae?v=fc6a00ebe8fc4c318a4f94b1959fe03f" target="_blank">страничка с упоминаниями</a> обо мне,<br> моих коллегах и наших совместных проектах.
+        А вот ещё и <a href="https://polyakovin.notion.site/e62ff7b80e3d4fec85cde132852386ae?v=fc6a00ebe8fc4c318a4f94b1959fe03f" target="_blank">страничка с упоминаниями</a> обо мне,<br> моих коллегах и наших совместных проектах.
       </p>
     `,
     en: `
       <p>
         This is a small portal where you can find
         <ul>
-          <li>my <a href="https://humorous-myrtle-078.notion.site/b4761a0713ff40cc98f842fe502c8145?v=fff551ba13a64bd8803bca5a376eb090" target="_blank">publications</a>,</li>
+          <li>my <a href="https://polyakovin.notion.site/b4761a0713ff40cc98f842fe502c8145?v=fff551ba13a64bd8803bca5a376eb090" target="_blank">publications</a>,</li>
           <li>the projects I worked on,</li>
           <li>and <a href="#contacts">contacts</a> to reach me out.</li>
         </ul>
       </p>
       <p>
-        And here is <a href="https://humorous-myrtle-078.notion.site/e62ff7b80e3d4fec85cde132852386ae?v=fc6a00ebe8fc4c318a4f94b1959fe03f " target="_blank">a page with mentions</a> about me,<br> my colleagues and our joint projects.
+        And here is <a href="https://polyakovin.notion.site/e62ff7b80e3d4fec85cde132852386ae?v=fc6a00ebe8fc4c318a4f94b1959fe03f " target="_blank">a page with mentions</a> about me,<br> my colleagues and our joint projects.
       </p>
     `,
   };
